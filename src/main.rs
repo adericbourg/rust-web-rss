28bf0cbe2d2458344aaac5cbd96785cb533a7bac
Alban Dericbourg <alban@dericbourg.net>
2019-02-09 18:27:12 +0100

    Configure RSS sources
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
 extern crate rocket_contrib;
+extern crate serde_yaml;
 
 #[cfg(test)]
 mod index_tests;
 
 use rocket_contrib::templates::Template;
+use std::env;
+use std::fs;
 
 #[derive(Serialize)]
 struct IndexContext {
@@ -28,5 +31,39 @@ fn rocket() -> rocket::Rocket {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let configuration_file = &args[1];
+    let configuration = load_configuration(configuration_file);
+    fetch_data(configuration.subscriptions);
     rocket().launch();
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Configuration {
+    pub subscriptions: Vec<Subscription>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Subscription {
+    pub name: String,
+    pub url: String,
+}
+
+pub fn load_configuration(configuration_file: &str) -> Configuration {
+    println!("Loading configuration file: '{}'", configuration_file);
+    let file_content = read_file(configuration_file);
+    let configuration: Configuration = serde_yaml::from_str(&file_content).unwrap();
+
+    configuration
+}
+
+fn read_file(file_name: &str) -> String {
+    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
+    contents
+}
+
+fn fetch_data(sources: Vec<Subscription>) {
+    for source in sources.iter() {
+        println!("Fetching RSS for '{}' ({})", source.name, source.url);
+    }
 }
