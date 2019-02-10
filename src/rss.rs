5be194104752d34b2acd377768fcf7675e101282
Alban Dericbourg <alban@dericbourg.net>
2019-02-10 15:32:10 +0100

    Fetch RSS data
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/rss.rs
+++ src/rss.rs
@@ -1,7 +1,28 @@
+extern crate reqwest;
+
 use crate::configuration;
 
-pub fn fetch_data(sources: Vec<configuration::Subscription>) {
-    for source in sources.iter() {
-        println!("Fetching RSS for '{}' ({})", source.name, source.url);
+pub struct Podcast {
+    pub name: String,
+    pub items: Vec<PodcastItem>,
+}
+
+pub struct PodcastItem {
+    title: String,
+    // TODO parse date
+    published: String,
+    media_url: String,
+}
+
+pub fn fetch_data(subscriptions: Vec<configuration::Subscription>) -> Vec<Podcast> {
+    for subscription in subscriptions.iter() {
+        fetch(subscription);
     }
+    vec!()
+}
+
+fn fetch(subscription: &configuration::Subscription) {
+    let mut response = reqwest::get(&subscription.url).expect("Failed to fetch subscription");
+    let text = response.text().expect("Failed to get response content");
+    println!("{:?}", text);
 }
