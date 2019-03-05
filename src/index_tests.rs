ff58ff5d6039617ed40109957a5285bc61b39af1
Alban Dericbourg <alban@dericbourg.net>
2019-03-05 12:01:24 +0100

    Fix (...) test
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/index_tests.rs
+++ src/index_tests.rs
@@ -7,7 +7,7 @@ use rocket_contrib::templates::Template;
 
 macro_rules! dispatch {
     ($method:expr, $path:expr, $test_fn:expr) => {{
-        let client = Client::new(rocket()).unwrap();
+        let client = Client::new(rocket(vec![])).unwrap();
         $test_fn(&client, client.req($method, $path).dispatch());
     }};
 }
@@ -15,7 +15,7 @@ macro_rules! dispatch {
 #[test]
 fn test_index() {
     dispatch!(Get, "/", |client: &Client, mut response: LocalResponse| {
-        let context = IndexContext { title: "Hello!" };
+        let context = IndexContext { title: "Podcasts", podcasts: &vec![] };
 
         let expected = Template::show(client.rocket(), "index", &context).unwrap();
         assert_eq!(response.status(), Status::Ok);
